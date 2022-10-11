5030b2cf9b8d2be5e7cc01c6d23ba47a76110e16
Thiago 02 <thiago.ralves02@gmail.com>
2022-10-10 22:34:43 -0300

    feat: componente footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FooterContainer } from './style'
+
+export default function Footer() {
+    return (
+        <FooterContainer>
+            <span>Criado por Thiago Alves</span>
+        </FooterContainer>
+    )
+}
---
+++ src/components/Footer/style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const FooterContainer = styled.footer`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    position: fixed;
+    bottom: 0;
+    background-color: var(--cor-bg-principal);
+    display: flex;
+    align-items: center;
+    height: 40px;
+    width: 100%;
+`
