47aa4ab4f4b6136c03e89e2fccc79763943c13a1
Thiago 02 <thiago.ralves02@gmail.com>
2022-10-10 22:34:14 -0300

    feat: pagina de ofertas

--- src/pages/Offers/Offers.jsx
+++ src/pages/Offers/Offers.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Api } from '../../api/Api'
+import Offer from '../../components/Offer/Offer'
+import offers from '../../api/offers.json'
+import { OffersContainer, Offerss } from './style'
+import Button from '../../components/Button/Button'
+import { useNavigate } from 'react-router-dom';
+import { Context } from '../../context/CtxApp'
 
 export default function Offers() {
+    const { endereco } = useContext(Context)
+    const navigate = useNavigate();
+
+    const redirectHome = () => {
+        navigate('/');
+    }
+
     return (
-        <div>Offers</div>
+        <>
+            <OffersContainer>
+                <span>Ofertas disponíveis no endereço : {endereco}</span>
+                <Offers>
+                    {
+                        offers?.map((offer, index) =>
+                            <Offer
+                                key={index}
+                                title={offer.title}
+                                image={offer.image}
+                                text={offer.text}
+                            />
+                        )
+                    }
+                </Offers>
+            </OffersContainer>
+            <Button
+                label="Ops, errei meu CEP!"
+                name="voltaHome"
+                onClick={redirectHome}
+            />
+        </>
     )
 }
--- src/pages/Offers/style.jsx
+++ src/pages/Offers/style.jsx
@@ -1,4 +1,27 @@
 import styled from "styled-components"
+import { media } from "../../assets/style/media"
 
 export const OffersContainer = styled.div`
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    span:first-child {
+        text-align: center;
+        font-size: 22px;
+        padding: 20px;
+        font-weight: bold;
+    }
+`
+export const Offers = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    ${media("desktop")}{
+        flex-direction: row;
+    }
 `
